36977e8278220bdcd9c73846a1ffaddcb7769700
takasu-atsuki <jill.perry0208@ilcoud.com>
2024-08-13 14:26:38 +0900

    second

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,18 @@
+'use client';
+
 import { getProviders, signIn } from 'next-auth/react';
 import ProviderOnClickButton from '../components/ProviderOnClickButton';
+import { useEffect, useState } from 'react';
 
-const Login = async () => {
-  const providers = await getProviders();
+const Login = () => {
+  const [providers, setProviders] = useState<any>(null);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  }, []);
 
   return (
     <div className="flex items-center justify-center py-16 px-4 sm:px-6 lg:px-8">
